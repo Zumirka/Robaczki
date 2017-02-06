d316114678bc211b2ec27079c54b977117b7a164
Zumirka <dominika.bartek1207@gmail.com>
2017-02-06 21:08:44 +0100

    Create class Insect

--- app/src/main/java/com/example/zumirka/robaczki/Game.java
+++ app/src/main/java/com/example/zumirka/robaczki/Game.java
@@ -4,24 +4,71 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.Timer;
+
 
 public class Game  extends View implements View.OnClickListener{
 
     private Insect insect;
+    Paint p = new Paint();
+    Handler h;
+    Runnable runnable;
+
 
     public Game(Context context, AttributeSet attrs) {
         super(context,attrs);
         insect=new Insect(100,100,30,80);
+        this.setOnClickListener(this);
     }
     public void onDraw(Canvas c) {
-
+       insect.setMove(5,5);
         insect.drawInsect(c);
-    }
 
+
+
+    }
     @Override
     public void onClick(View v) {
+
+
+        h = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                postInvalidate();
+                h.postDelayed(this, 1);
+
+            }
+        };
+        h.postDelayed(runnable, 1);
+            return;
+
+
+
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        p.setColor(Color.RED);
+        p.setTextSize(50);
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_MOVE:
+                //delete insect
+                moveInsect();
+
+        }
+        return false;
+    }
+    public void moveInsect()
+    {
 
     }
 }
--- app/src/main/java/com/example/zumirka/robaczki/Insect.java
+++ app/src/main/java/com/example/zumirka/robaczki/Insect.java
@@ -8,16 +8,40 @@ import android.graphics.RectF;
 
 public class Insect {
     private RectF oval;
+    private int x,y,w,h,move_x,move_y;
     public Insect(int x,int y,int w,int h)
     {
-
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
         oval=new RectF(x,y,x+w,y+h);
-
-
     }
     public RectF getRect(){
 
+        float x=(this.x+move_x );
+        float y=( this.y+move_y) ;
+        oval.left=x;
+        oval.top= y;
+        oval.bottom=x+w;
+        oval.right=y+h;
         return this.oval;
+    }
+    public int  getX()
+    {
+        return x;
+    }
+    public  int getY()
+    {
+        return y;
+    }
+    public  int getW()
+    {
+        return w;
+    }
+    public  int getH()
+    {
+        return h;
     }
     public  void drawInsect(Canvas c)
     {
@@ -27,6 +51,14 @@ public class Insect {
         paint.setColor(Color.RED);
 
         c.drawOval(getRect(), paint);
+    }
+    public void setMove(int move_x,int move_y)
+    {
+        this.move_x=move_x;
+        this.move_y=move_y;
+        x+=move_x;
+        y+=move_y;
+
     }
 
 }
